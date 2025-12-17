deb385a4fc90c81a6976d6d18acd25d9c98068cb
Josue Ruiz <andyjosue123.jr@gmail.com>
2025-12-16 20:31:03 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    // Las siguientes líneas no tienen que ser necesariamente 7, puedes cambiarlas a 4, 2 o las que quieras
+    lines: [
+         "Para el amor de mi vida:",
+         "Si pudiera elegir un lugar seguro, sería a tu lado. Cuanto más tiempo estoy contigo más te amo. Gracias por ser mi hogar, mi paz y mi felicidad."
+    ],
+  
+    date: "2023-12-16T00:00:00",
+    // Nombres de ambos
+    names: ["Mi amor por ti empezo hace"]
+}
